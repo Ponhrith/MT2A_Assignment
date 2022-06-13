0a57c60a4b055cba841b40de093903c2577e1ed8
Ponhrith <ponhrith.nhep@gmail.com>
2022-06-13 21:29:36 +0700

    done

--- peytonv2.py
+++ peytonv2.py
@@ -1,22 +1,23 @@
 totalAmount = 0.0
-print("IN THIS INVESTMENT WE EXPECT TO HAVE EXACT 21 YEARS OF GOOD RETURN AND 9 YEARS OF BAD RETURN")
-def earn():
-    print("PRESS 1 TO SEE THE GOOD RETURN:")  #peyton calculation is divided into two parts so i gave the users options
-    print("PRESS 2 TO SEE THE BAD RETURN:")   #giving them options not to make the program messy
+print("IN THIS INVESTMENT WE EXPECT TO HAVE EXACT 9 YEARS OF BAD RETURN AND 21 YEARS OF GOOD RETURN")
+def peytoncal():
+# def earn():
+#     print("PRESS 1 TO SEE THE GOOD RETURN:")  #peyton calculation is divided into two parts so i gave the users options
+#     print("PRESS 2 TO SEE THE BAD RETURN:")   #giving them options not to make the program messy
 
-def peytoncal(): #define the main function asking them to choose
-    parAmount = float(0.0)
-    loop = True
-    while loop:
-        earn()
-        see = input("Please select Option 1 or 2:")  
-        if see == '1':
-            good()
-        elif see == '2':
+# def peytoncal(): #define the main function asking them to choose
+        parAmount = float(0.0)
+#     loop = True
+#     while loop:
+#         earn()
+#         see = input("Please select Option 1 or 2:")  
+#         if see == '1':
+#        good()
+#         elif see == '2':
             
-            good(bad(parAmount))
-        else: 
-            print("Invalid Option. Please select Option 1 or 2.")
+        good(bad(parAmount))
+        # else: 
+        #     print("Invalid Option. Please select Option 1 or 2.")
 
 def good(goodAmount): #define good function for the good return for the 21 years
     amount = goodAmount #totalAmount # int(250000) # I assigned them value, so to let it run autmoatically when the user select any option
@@ -30,7 +31,7 @@ def good(goodAmount): #define good function for the good return for the 21 years
             monthly.append(amount * (1 + i_r /100) ** (i + 1))
         print("The Total Value to Date is:", round(monthly[i], 2))
     table = []
-    print("SUPPOSED THE FIRST 21 YEARS IS THE GOOD RETURN OF " + str(amount) +
+    print("SUPPOSED THE LAST 21 YEARS IS THE GOOD RETURN OF"  + str(amount)  +
           "EVERY YEAR AT AN INTERST RATE OF" + str(i_r) + "%")
     for i in range(duration):
         table.append([i + 1, round(monthly[i])])
