2fe83430649d64f23fd82130d0fe3908432ea1a3
Ponhrith <ponhrith.nhep@gmail.com>
2022-09-22 13:17:19 +0700

    Testing

---
+++ Project1C/Nhep-Ponhrith-Toyota Case.py
@@ -0,0 +1,5 @@
+supplier = ["VN", "Thai"]
+supplyCapacity = {"VN": 200, "B": 300}
+demandNodes = ["KCham", "SR", "KThom", "PP", "BTB"]
+demandUnit = {"KCham": 100,"SR": 80,"KThom": 60, "PP": 170, "BTB": 70}
+
