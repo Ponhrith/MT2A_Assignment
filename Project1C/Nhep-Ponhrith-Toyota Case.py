560230747ddd7c7e4e0e21a4f54308257bcc495f
Ponhrith <ponhrith.nhep@gmail.com>
2022-09-23 13:59:27 +0700

    Test

--- Project1C/Nhep-Ponhrith-Toyota Case.py
+++ Project1C/Nhep-Ponhrith-Toyota Case.py
@@ -1,5 +1,22 @@
 supplier = ["VN", "Thai"]
 supplyCapacity = {"VN": 200, "B": 300}
 demandNodes = ["KCham", "SR", "KThom", "PP", "BTB"]
-demandUnit = {"KCham": 100,"SR": 80,"KThom": 60, "PP": 170, "BTB": 70}
+demandUnit = {"KCham": 100,
+            "SR": 80,
+            "KThom": 60, 
+            "PP": 170, 
+            "BTB": 70}
 
+intermediateNodes = ["KCham", "SR", "PP", "BTB"]
+
+intermediateNodesCost = [
+    [30,40]
+    [50,45,50]
+]
+
+intermediateNodesCostSend = [
+    [50]
+    [30]
+    [40]
+    [35]
+]
